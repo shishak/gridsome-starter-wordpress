02046568252821e79df5cd8318f731b094b73995
Shishak <semenshishak@gmail.com>
2019-10-04 20:51:36 +0300

    Update SiteFooter.vue

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,5 +1,58 @@
 <template>
-  <footer class="bg-white text-center py-5 mt-5">
-    Built with <a href="https://gridsome.org/">Gridsome</a> <span class="font-family-headings"> &amp; </span> <a href="https://bootstrap-vue.js.org/">BootstrapVue</a>
-  </footer>
+ <footer>
+  <div class="footer-container">
+    <section class="footer-main">
+      <article>
+       <h3>Learn</h3>
+        <ul>
+          <li><a href="#!">Guides</a></li>
+          <li><a href="#!">Reference</a></li>
+          <li><a href="#!">Samples</a></li>
+          <li><a href="#!">Libraries</a></li>
+          <li><a href="#!">Github</a></li>
+        </ul>
+      </article>
+      <article>
+        <h3>Learn</h3>
+        <ul>
+          <li><a href="#!">Guides</a></li>
+          <li><a href="#!">Reference</a></li>
+          <li><a href="#!">Samples</a></li>
+          <li><a href="#!">Libraries</a></li>
+          <li><a href="#!">Github</a></li>
+        </ul>
+      </article>
+      <article>
+        <h3>Stay connected</h3>
+        <ul>
+          <li><a href="#!">Blog</a></li>
+          <li><a href="#!">Firebase Summit</a></li>
+          <li><a href="#!">Facebook</a></li>
+          <li><a href="#!">Twitter</a></li>
+          <li><a href="#!">YouTube</a></li>
+        </ul>
+      </article>
+      <article>
+        <h3>Support</h3>
+        <ul>
+          <li><a href="#!">Contact support</a></li>
+          <li><a href="#!">Stack Overflow</a></li>
+          <li><a href="#!">Google Group</a></li>
+          <li><a href="#!">Release notes</a></li>
+          <li><a href="#!">FAQs</a></li>
+          <li><a href="#!">Legacy documentation</a></li>
+        </ul>
+      </article>
+    </section>
+    <hr>
+    <section class="footer-bottom">
+      <small>© 2019 Google. All rights reserved.
+      </small>
+      <ul>
+        <li><a href="#!">Facebook</a></li>
+        <li><a href="#!">Twitter</a></li>
+      </ul>
+    </section>
+  </div>
+</footer>
 </template>

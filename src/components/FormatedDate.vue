f921a4b0147c5abcf50a2f7e703bb35630780c82
Shishak <semenshishak@gmail.com>
2019-10-04 22:13:23 +0300

    Update FormatedDate.vue

--- src/components/FormatedDate.vue
+++ src/components/FormatedDate.vue
@@ -13,8 +13,32 @@ export default {
   },
   computed: {
     formatedDate() {
-      return moment(this.date).fromNow().locale('uk_UA');
+      return moment(this.date).fromNow();
     }
   }
 }
+
+
+moment.updateLocale('en', {
+    relativeTime : {
+        past: function(input) {
+          return input === 'just now'
+            ? input
+            : input + ' ago'
+        },
+        s  : 'just now',
+        future: "in %s",
+        ss : '%d seconds',
+        m:  "a minute",
+        mm: "%d minutes",
+        h:  "an hour",
+        hh: "%d hours",
+        d:  "a day",
+        dd: "%d days",
+        M:  "a month",
+        MM: "%d months",
+        y:  "a year",
+        yy: "%d years"
+    }
+});
 </script>

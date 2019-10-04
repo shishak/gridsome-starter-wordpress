2af605fa7ed754f8ec9b13a0728b4401d3726a9e
Shishak <semenshishak@gmail.com>
2019-10-04 21:10:36 +0300

    Update SiteSidebar.vue

--- src/components/SiteSidebar.vue
+++ src/components/SiteSidebar.vue
@@ -2,7 +2,6 @@
   <b-col lg="3" class="bg-light mt-4 mt-lg-5 p-4 rounded">
     <aside class="mb-5">
       <h2 class="mb-3 font-family-sans-serif h4">About</h2>
-      <p>Hello! You're looking at a <a href="https://bootstrap-vue.js.org">BootstrapVue</a> powered <a href="https://gridsome.org">Gridsome</a> starter that uses WordPress for content. Let me tell you that Gridsome is simply awesome!  Happy coding! <span class="text-primary">&hearts;</span></p>
     </aside>
     <tags-widget class="mb-5"/>
     <categories-widget class="mb-5"/>

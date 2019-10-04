18ffd743f07e9abc5ca6d54c576b214ec740b243
Shishak <semenshishak@gmail.com>
2019-10-04 06:06:14 +0300

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,7 +7,7 @@ module.exports = {
       options: {
         baseUrl: process.env.WORDPRESS_URL, // required
         typeName: 'WordPress', // GraphQL schema name (Optional)
-        perPage: 100, // How many posts to load from server per request (Optional)
+        perPage: 10, // How many posts to load from server per request (Optional)
         concurrent: 10, // How many requests to run simultaneously (Optional)
         routes: {
           post: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)

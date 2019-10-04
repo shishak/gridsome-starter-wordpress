4943a9f4cddb85a44b02f1ca744df2a354b64069
Shishak <semenshishak@gmail.com>
2019-10-04 21:50:12 +0300

    Update Post.vue

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -2,11 +2,16 @@
    <g-link :to="post.path" class="grid-item">
     <div v-html="post.content" />
     <h2 v-html="post.title" />
+    <formated-date class="post-date font-weight-bold" :date="post.date"/>
    </g-link>     
 </template>
 
 <script>
+import FormatedDate from "~/components/FormatedDate.vue"
 export default {
+  components: {
+    FormatedDate
+  },
   props: {
     post: {
       type: Object,

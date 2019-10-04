96ad5570b4d67381a2f459f5781f1f82c071103d
Shishak <semenshishak@gmail.com>
2019-10-04 06:13:02 +0300

    Update Post.vue

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,7 +3,7 @@
    <g-link :to="post.path">
     <h2 v-html="post.title" />
      </g-link>
-       <div v-html="post.excerpt" />
+       <div v-html="$page.wordPressPost.content"/>
   </div>
 </template>
 

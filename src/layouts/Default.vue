03c25b0a7bc0eecdde59bf64878876c47946987d
Shishak <semenshishak@gmail.com>
2019-10-04 20:22:33 +0300

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -11,63 +11,6 @@
     </header>
     <slot/>
   </div>
-  <footer>
-  <div class="footer-container">
-    <section class="footer-main">
-      <article>
-       <h3>Learn</h3>
-        <ul>
-          <li><a href="#!">Guides</a></li>
-          <li><a href="#!">Reference</a></li>
-          <li><a href="#!">Samples</a></li>
-          <li><a href="#!">Libraries</a></li>
-          <li><a href="#!">Github</a></li>
-        </ul>
-      </article>
-      <article>
-        <h3>Learn</h3>
-        <ul>
-          <li><a href="#!">Guides</a></li>
-          <li><a href="#!">Reference</a></li>
-          <li><a href="#!">Samples</a></li>
-          <li><a href="#!">Libraries</a></li>
-          <li><a href="#!">Github</a></li>
-        </ul>
-      </article>
-      <article>
-        <h3>Stay connected</h3>
-        <ul>
-          <li><a href="#!">Blog</a></li>
-          <li><a href="#!">Firebase Summit</a></li>
-          <li><a href="#!">Facebook</a></li>
-          <li><a href="#!">Twitter</a></li>
-          <li><a href="#!">YouTube</a></li>
-        </ul>
-      </article>
-      <article>
-        <h3>Support</h3>
-        <ul>
-          <li><a href="#!">Contact support</a></li>
-          <li><a href="#!">Stack Overflow</a></li>
-          <li><a href="#!">Google Group</a></li>
-          <li><a href="#!">Release notes</a></li>
-          <li><a href="#!">FAQs</a></li>
-          <li><a href="#!">Legacy documentation</a></li>
-        </ul>
-      </article>
-    </section>
-    <hr>
-    <section class="footer-bottom">
-      <small>© 2019 Google. All rights reserved.
-      </small>
-      <ul>
-        <li><a href="#!">Facebook</a></li>
-        <li><a href="#!">Twitter</a></li>
-      </ul>
-    </section>
-  </div>
-</footer>
-  
 </template>
 
 <style>

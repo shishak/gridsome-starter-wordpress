48cde815bda0bd401166b369d0676d85421200d9
Shishak <semenshishak@gmail.com>
2019-10-04 18:45:23 +0300

    Update Post.vue

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -2,7 +2,7 @@
    <g-link :to="post.path" class="grid-item">
     <div v-html="post.content" />
     <h2 v-html="post.title" />
-    <span :date="post.date"/>
+    <span v-html="post.date"/>
    </g-link>     
 </template>
 

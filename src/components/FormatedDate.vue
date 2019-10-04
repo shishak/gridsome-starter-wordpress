c3b91773d9d3502a27523b2b7870c94f6ae399b0
Shishak <semenshishak@gmail.com>
2019-10-04 21:55:31 +0300

    Update FormatedDate.vue

--- src/components/FormatedDate.vue
+++ src/components/FormatedDate.vue
@@ -13,7 +13,7 @@ export default {
   },
   computed: {
     formatedDate() {
-      return moment(this.date).locale('ua_uk').format('ll')
+      return moment(this.date).locale('uk_UA').format('ll')
     }
   }
 }
